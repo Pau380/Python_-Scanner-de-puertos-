5b9b528716c369957fa7df6b0540d58f809820bd
Pau_Vlz <pau.dvelez8087@gmail.com>
2025-08-07 19:43:05 -0500

    Add files via upload

---
+++ scanner_puertos .py
@@ -0,0 +1,27 @@
+#scanner de puertos
+#se requiere controlar la red desde Python, se debe importar la libreria socket 
+#socket nos sirve para crear conecciones
+import socket
+
+#se requiere poner la dirección ip que se desea escanear mi ip= 192.168.80.17
+
+ip= input("Ingrese la dirección IP a escanear: ")
+
+#Hay que hacer un bucle for para escaear los puertos
+#Hay 65.965 puertos
+#socket.SOCK_STREAM: significa que estamos utilizando el protocolo TCP
+
+for puerto in range (1,65535):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(5)
+
+#vamos a utilizar el socket para ver que puerto está abierto o no
+    result = sock.connect_ex((ip, puerto))
+
+    #sí el puerto está aierto
+    if result == 0:
+        print("Puerto abierto: " + str(puerto))
+        sock.close() #cerramos el socket
+    else:
+        print("Puerto cerrado: " + str(puerto))
+
